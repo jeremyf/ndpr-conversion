ca65cf2323d583cd92a65947b2e56e81d49f8f23
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-03-11 15:01:44 -0500

    Updated load assets to better handle the URI

--- lib/upload_assets.rb
+++ lib/upload_assets.rb
@@ -2,7 +2,6 @@
 
 require 'yaml'
 require 'rest_client'
-require 'mime/types'
 require 'fileutils'
 
 @username = ''
@@ -20,12 +19,13 @@ config.each do |key, attributes|
       RestClient.post("https://#{@username}:#{@password}@#{@host}/admin/assets", {"asset" => { "file" => File.new(File.expand_path(upload_filename)), 'tag' => 'imported' }})
     rescue RestClient::Found => e
       uri = URI.parse(e.response.headers[:location])
-      attributes[:conductor] = uri
+      path = uri.path.sub(/\/admin\/assets\//)
+      attributes[:conductor] = File.join('/', path, File.basename(upload_filename))
     end
   end
 end
 
-
+# Lets write down where things went
 File.open(config_file, "w+") do |file|
   file.puts YAML.dump(config)
 end
